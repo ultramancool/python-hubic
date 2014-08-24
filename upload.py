e7ae4c8700dc2668a1e5ec742928248ec1bfbe79
root <root@cmadm>
2014-08-24 14:46:17 -0400

    moved all into one script, added logout

--- upload.py
+++ upload.py
@@ -1,14 +1,63 @@
-from swiftclient import Connection
-import os
-import sys
+#!/usr/bin/env python
+# originally (c) 2013 Antoine Sirinelli <antoine@monte-stello.com>
+# hacked up a bunch after that though. 
 
-if len(sys.argv) < 4:
-	print "usage: script storage_url auth_token file_to_upload"
+import requests
+import json
+from base64 import b64decode
+import sys
+import os
+
+class auth_hubic:
+    def __init__(self, user, passwd):
+        self.SessionHandler = 'https://ws.ovh.com/sessionHandler/r4/'
+        self.hubicws = 'https://ws.ovh.com/hubic/r5/'
+
+        r = requests.get(self.SessionHandler + 'rest.dispatcher/' + 'getAnonymousSession')
+        sessionId = r.json()['answer']['session']['id']
+
+        params = { 'sessionId': sessionId,
+                   'email': user}
+        payload = {'params': json.dumps(params)}
+
+        r = requests.get(self.hubicws + 'rest.dispatcher/' + 'getHubics',
+                         params=payload)
+        hubics = r.json()
+        self.hubicsId = hubics['answer'][0]['id']
+
+        params = { 'login': hubics['answer'][0]['nic'],
+                   'password': passwd,
+                   'context': 'hubic'}
+        payload = {'params': json.dumps(params)}
+
+        r = requests.get(self.SessionHandler + 'rest.dispatcher/' + 'login',
+                         params=payload)
+
+        self.sessionId = r.json()['answer']['session']['id']
+
+    def get_credentials(self):
+        params = { 'sessionId': self.sessionId,
+                   'hubicId': self.hubicsId}
+        payload = {'params': json.dumps(params)}
+
+        r = requests.get(self.hubicws + 'rest.dispatcher/' + 'getHubic',
+                         params=payload)
+        Storage_Url = b64decode(r.json()['answer']['credentials']['username'])
+        Auth_Token = r.json()['answer']['credentials']['secret']
+        return Storage_Url, Auth_Token
+
+    def logout(self):
+        params = { 'sessionId': self.sessionId}
+        payload = {'params': json.dumps(params)}
+        r = requests.get(self.SessionHandler + 'rest.dispatcher/' + 'logout',
+                         params=payload)
+
+if (len(sys.argv) < 3):
+	print "usage: script user password"
 	sys.exit(1)
 
-options = {'auth_token': sys.argv[2],
-           'object_storage_url': sys.argv[1]}
-conn = Connection(os_options=options, auth_version=2)
-
-conn.put_object("default/", os.path.basename(sys.argv[3]), open(sys.argv[3],'rb'))
-
+user = sys.argv[1]
+passwd = sys.argv[2]
+hubic = auth_hubic(user, passwd)
+storage_url, auth_token = hubic.get_credentials()
+print storage_url, auth_token
